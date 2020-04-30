720f96e45a2a9592eedcc34d0e70e981f114fac5
Takuma_Kitahara <takumakitahara.jp@gmail.com>
2020-04-30 22:18:32 +0900

    update buy.js + add rule.css

--- buy/buy.js
+++ buy/buy.js
@@ -2,27 +2,35 @@
 
 let rate = document.winner.rate;
 
+let money = 10000;
+
 document.getElementById('buy').onclick = function () {
-  // 0-2をランダムに生成
-  let number = Math.round(Math.random() * (rate.length -1));
+  //勝ち馬番号をランダムに生成
+  let number = Math.round(Math.random() * (rate.length) + 1);
   let buy = confirm('購入しますか?');
   // 購入確認
   if(buy == true) {
-    for(let i = 0; i < rate.length; i++) {
-      if(rate[i].checked == true) {
-        if(rate[i].value == number) {
-          console.log('当たり');
-          console.log(number);
+    let i = 0;
+    for(let j = 0; j < rate.length; j++) {
+      if(rate[j].checked == true) {
+        if(rate[j].value == number) {
+          //当たり処理
+          money = money * rate[j].value //*oddz
+          // location.href = 'file:///E:/js/keiba-game/result/result.html'
         } else {
-          console.log('はずれ');
-          console.log(number);
+          //ハズレ処理
+          // location.href = 'file:///E:/js/keiba-game/result/result.html'
         }
-      } 
+      } else {
+        i ++;
+      }
+    }
+    if(i == rate.length) {
+      alert('買う馬を決めてください');
     }
   }
-  // location.href = 'file:///E:/js/keiba-game/result/result.html'
+  document.getElementById('money').textContent = '￥' + money;
 }
-
 
 document.getElementById('clear').onclick = function () {
   for(let i = 0; i < rate.length; i++) {
