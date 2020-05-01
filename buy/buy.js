01c3679a5f2de44d5dd3e8b48787f795d86a2a1e
Takuma_Kitahara <takumakitahara.jp@gmail.com>
2020-05-01 23:03:52 +0900

    update calc oddz

--- buy/buy.js
+++ buy/buy.js
@@ -3,6 +3,24 @@
 let rate = document.winner.rate;
 
 let money = 10000;
+document.getElementById('money').textContent = '￥' + money;
+
+let oddz = {
+  list: [
+    {
+      value:null,
+    },
+    {
+      value:9.5,
+    },
+    {
+      value:1.6,
+    },
+    {
+      value:3.4,
+    },
+  ],
+};
 
 document.getElementById('buy').onclick = function () {
   //勝ち馬番号をランダムに生成
@@ -10,18 +28,21 @@ document.getElementById('buy').onclick = function () {
   let buy = confirm('購入しますか?');
   // 購入確認
   if(buy == true) {
+    //購入金額を指定
     let i = 0;
     for(let j = 0; j < rate.length; j++) {
       if(rate[j].checked == true) {
         if(rate[j].value == number) {
           //当たり処理
-          money = money * rate[j].value //*oddz
+          money = money + (money * oddz.list[j].value);
           // location.href = 'file:///E:/js/keiba-game/result/result.html'
         } else {
           //ハズレ処理
+          money = money + (money * 0);
           // location.href = 'file:///E:/js/keiba-game/result/result.html'
         }
       } else {
+        //チェック確認
         i ++;
       }
     }
